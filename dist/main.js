b24e49360991eec22b967161f9706290ab9a4283
Denis <blisterrabbithutch@gmail.com>
2020-04-30 15:44:02 +0500

    created cv page

--- dist/main.js
+++ dist/main.js
@@ -91,20 +91,5 @@
 "use strict";
 
 
-$('.business-card__info-item--phone').on('click', function (evt) {
-  var phoneButton = $(this);
-  var phoneNumber = $(this).attr('data-number');
-
-  if (phoneButton.attr('data-opened') == 'false') {
-    evt.preventDefault();
-    phoneButton.addClass('business-card__info-item--faded');
-    setTimeout(function () {
-      phoneButton.attr('data-opened', 'true');
-      phoneButton.find('.business-card__info-item-title').text(phoneNumber);
-      phoneButton.removeClass('business-card__info-item--faded');
-    }, 500);
-  }
-});
-
 /***/ })
 /******/ ]);
--- js/entry.js
+++ js/entry.js
@@ -1,16 +1,2 @@
 'use strict';
 
-$('.business-card__info-item--phone').on('click', function(evt) {
-  let phoneButton = $(this);
-  let phoneNumber = $(this).attr('data-number');
-  if (phoneButton.attr('data-opened') == 'false') {
-    evt.preventDefault();
-    phoneButton.addClass('business-card__info-item--faded');
-    setTimeout(function()
-    {
-      phoneButton.attr('data-opened', 'true');
-      phoneButton.find('.business-card__info-item-title').text(phoneNumber);
-      phoneButton.removeClass('business-card__info-item--faded');
-    }, 500);
-  }
-});
